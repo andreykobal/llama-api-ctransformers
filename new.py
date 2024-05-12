a2ab081717e6b92c12c228c88a34a0d1f42d2574
Andreas Kobal <kobalandrey@gmail.com>
2024-05-12 22:06:02 +0100

    add model

--- new.py
+++ new.py
@@ -4,14 +4,16 @@ from ctransformers import AutoModelForCausalLM
 app = Flask(__name__)
 
 # Load the model (this should be done once, not on every request)
-llm = AutoModelForCausalLM.from_pretrained("TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ", gpu_layers=50)
+#llm = AutoModelForCausalLM.from_pretrained("TheBloke/Wizard-Vicuna-7B-Uncensored-GPTQ", gpu_layers=50)
+llm = AutoModelForCausalLM.from_pretrained("TheBloke/LLaMA2-13B-Tiefighter-GPTQ", gpu_layers=50)
+
 
 @app.route('/generate', methods=['POST'])
 def generate_text():
     content = request.json
     prompt = content['prompt']
     response = llm(prompt)
-    print(response.encode('utf-8').decode('utf-8'))
+    print(response)
     return jsonify({"response": response})
 
 if __name__ == '__main__':
