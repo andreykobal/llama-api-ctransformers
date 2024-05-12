2f79dbd6e1c5bfcefe20d50dd639aa3193f20a2d
Andreas Kobal <kobalandrey@gmail.com>
2024-05-12 22:31:50 +0100

    smallest llama

--- python.py
+++ python.py
@@ -4,13 +4,14 @@ from ctransformers import AutoModelForCausalLM
 app = Flask(__name__)
 
 # Load the model (this should be done once, not on every request)
-llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGML", model_file="llama-2-7b-chat.ggmlv3.q4_K_M.bin", gpu_layers=50)
+llm = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-7B-Chat-GGML", model_file="llama-2-7b-chat.ggmlv3.q2_K.bin", gpu_layers=50)
 
 @app.route('/generate', methods=['POST'])
 def generate_text():
     content = request.json
     prompt = content['prompt']
     response = llm(prompt)
+    print(response)
     return jsonify({"response": response})
 
 if __name__ == '__main__':
